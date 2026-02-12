bde8a25401db575e5a2177d34aac9776b9494336
Alex <alex@local>
2026-02-12 23:44:53 +0800

    Simplify to bare minimum Android project

---
+++ app/src/main/java/com/alex/bridge/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alex.bridge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Bridge Service Running");
+        setContentView(tv);
+    }
+}
